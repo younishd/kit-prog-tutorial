a592556babe4551c619931230a92220e97687e27
YouniS Bensalah <younis.bensalah@riseup.net>
2015-11-20 02:12:58 +0100

    omgz

--- snippets/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String argv[]) {
-        Product p = new Product();
-        p.display();
-
-        Dog odie = new Dog("Odie");
-        odie.sayName();
-    }
-}
